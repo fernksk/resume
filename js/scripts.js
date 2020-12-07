182fb1c930305cea01281be28407afae786261fd
fernksk <fl8llia@gmail.com>
2020-12-06 17:49:13 -0800

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,2 @@
 console.log('Hello from js/scripts.js!');
-$(document).ready(function () { $('body').hide().fadeIn(2500).delay(8000)});
+$(document).ready(function () { $('body').hide().fadeIn(1500).delay(8000)});
